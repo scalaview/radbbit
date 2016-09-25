e3bd34900ce6d7e5995328610d58275a583a8ee4
scalaview <chailink100@gmail.com>
2016-09-25 20:48:26 +0800

    add client

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var app = express();
-app.set('port', process.env.PORT || 8008);
+app.set('port', process.env.PORT || 80);
 app.enable('verbose errors');
 
 app.use(express.query());
@@ -24,36 +24,21 @@ app.use("*", function(req, res, next) {
       mime = contentType.split(';')[0];
   var target = req.originalUrl
   if(target.toLowerCase().indexOf("http")== -1){
-    target = "http://" + req.headers.originhost + target
+    target = "http://pro.protchar.cn" + target
   }
   if (req.method == 'POST' || req.method == 'PUT') {
     var options = {
       headers: req.headers,
       url: target
     }
-    options.headers.host = req.headers.originhost
+    options.headers["originhost"] = req.headers.host
     if(mime.indexOf('x-www-form-urlencoded') != -1){
       options["form"] = req.body
     }else if(mime.indexOf('form-data') != -1){
       options["formData"] = req.body
     }
     request.post(options, function(err, hostres, body){
-      if (!err && hostres.statusCode == 200) {
-        var rdata = data = hostres.body.trim()
-        console.log(data)
-        try{
-          var rdata = JSON.parse(data)
-          if(rdata.LicenseResult != 'undefined' && rdata.LicenseResult != 1 ){
-            rdata.LicenseResult = 1
-          }
-        }catch(e){
-          console.log("json format error")
-        }
-        res.json(rdata)
-      }else{
-        console.log(err)
-        res.json(defaultResponse)
-      }
+      res.pipe(hostres)
     })
   }else if (req.method === 'GET' || req.method === 'HEAD') {
     request.get(target).pipe(res)
